9b09286eb066ecbd73ea47ddc3bac246397f577e
SummerQY <polarlights@mail.ustc.edu.cn>
2022-06-28 18:26:51 +0800

    更新了day的另外两个作业，分别是判断平年闰年和输出乘法口诀表

---
+++ day1/day1.c
@@ -0,0 +1,64 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+// 打印100~200之间的素数（prime number）
+//int check(int i) 
+//{
+//	int a;
+//	if (i % 2 == 0)
+//		return 0;
+//	else
+//	{
+//		for (a = 3; a <= ((i + 1) / 2); a++) 
+//		{
+//			if (i % a == 0)
+//				return 0;
+//		}
+//		return 1;
+//	}
+//}
+//
+//int main() 
+//{
+//	int a;
+//	printf("100~200之间的素数有：\n");
+//	for (a = 100; a <= 200; a++) 
+//	{
+//		if (check(a) != 0)
+//			printf("%d、", a);
+//	}	
+//	printf("\n\n");
+//	return 0;
+//}
+
+//输出乘法口诀表
+//#include<stdio.h>
+//int main()
+//{
+//  int a, b;
+//	printf("乘法口诀表：\n");
+//	for (b = 1; b <= 9; b++)
+//	{
+//		for (a = 1; a <= b; a++)
+//		{
+//			printf("%d*%d=%d    ", a, b, a * b);
+//		}
+//		printf("\n");
+//	}
+//	printf("\n");
+//	return 0;
+//}
+
+//check the leap year between 1000 and 2000
+int main() 
+{
+	int a;
+	printf("请输入要检查的年份：");
+	scanf("%d", &a);
+	if (a % 4 == 0)
+		printf("%d是闰年\n",a);
+	else
+		printf("%d是平年\n",a);
+	printf("\n");
+	return 0;
+}
+
