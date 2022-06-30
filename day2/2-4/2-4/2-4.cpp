66a37a47d9b59236b9bbb2c7c072ad99c4cd6178
SummerQY <polarlights@mail.ustc.edu.cn>
2022-06-30 16:14:50 +0800

    2-1测试

--- day2/2-4/2-4/2-4.cpp
+++ day2/2-4/2-4/2-4.cpp
@@ -1,28 +1,44 @@
 //将三个数按从大到小输出。
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-int rank(int arr[]) 
+void rank(int arr[],int len) 
 {
-
+	int max, loc;
+	int i, j;
+	for (i=0;i<=len-1;i++) 
+	{
+		max = arr[i], loc = i;
+		for (j =i+1;j<=len-1;j++ )
+		{
+			if (max < arr[j]) 
+			{
+				max = arr[j];
+				loc = j;
+			}
+		}
+		arr[loc] = arr[i];
+		arr[i] = max;
+	}
 }
 int main() 
 {
-	int in[3] = { 0 }, out[3] = { 0 };
-	int i, j, temp;
-	for (i = 0; i <= 2; i++) 
+	int len;
+	printf("please input arr length(1~100):");
+	scanf("%d", &len);
+	printf("\n");
+	int i, j;
+	int arr[100] = {0};
+	for (i=0;i<=len-1;i++) 
 	{
-		printf("please input integer%d:",i);
-		scanf("%d", &in[i]);
+		printf("input arr[%d]:", i);
+		scanf("%d", &arr[i]);
 		printf("\n");
-    }
-	temp = in[0];
-	if (in[0] < in[1])
-	{
-		in[0] = in[1];
-		in[1] = temp;
-		temp = in[0];
 	}
-		
-	  
-		
+	rank(arr, len);
+	printf("descending output:\n");
+	for(j = 0; j <= len - 1; j++) 
+	{
+		printf("%d、", arr[j]);
+	}
+	return 0;
 }
