ddd3e1d0026258a1db200084846f94de227504e2
SummerQY <polarlights@mail.ustc.edu.cn>
2022-07-03 09:37:02 +0800

    3-3AC！！

--- day3/3-3/3-3.c
+++ day3/3-3/3-3.c
@@ -1,39 +1,32 @@
 //编写程序数一下 1到 100 的所有整数中出现多少个数字9
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+#include<math.h>
 int checklen(int N) 
 {
 	int i;
-	if (N == 1)
-		return 1;
-	else
-		for (i = 1; 1; i++) 
-		{
-			if (N>=(10^(i-1))&&N<(10^i))
-				return i;
-		}
-}
-
-void d_arr(int D_arr[],int len,int N) 
-{
-	int i;
-	if (len == 1)
-		D_arr[0] = N;
-	else 
+	for (i = 1; i<=10; i++) 
 	{
-		for(i=1;i<=len;i++)
-		{
-			if (i != len )
-			{
-				D_arr[i - 1] = (N % (10 ^ i)) / (10 ^ (i - 1));
-				N = N - D_arr[i - 1] * 10 ^ (i - 1);
-			}
-			else
-				D_arr[i-1] = N / (10 ^ (i-1));
-		}
+		if (pow(10,i-1)<= N)
+			if (pow(10,i) > N)
+				return i;
 	}
 }
-
+void d_arr(int D_arr[],int len,int N) 
+{
+	int i,j;
+	j = N;
+	for (i = 1; i <= len; i++)
+	{
+		if (i != len)
+		{
+			D_arr[i - 1] = (j % (int)(pow(10,i))) / (pow(10,i-1));
+			j = j - D_arr[i - 1] * (pow(10,i-1));
+		}
+		else
+			D_arr[i - 1] = j / (pow(10,i-1));
+	}	
+}
 int count(int D_arr[], int len) 
 {
 	int count, i;
@@ -45,11 +38,10 @@ int count(int D_arr[], int len)
 	}
 	return count;
 }
-
-void arrclear(int A[],int l) 
+void arrclear(int A[],int a) 
 {
 	int i;
-	for (i = 0; i <= l; i++) 
+	for (i = 0; i < a; i++) 
 	{
 		A[i] = 0;
 	}
@@ -59,11 +51,14 @@ int main()
 {
 	int i,len, cou=0;
 	int D_arr[10] = {0};
+	int test;
+	test = pow(10,2);
+	printf("test=%d\n",test);
 	for (i = 1; i <= 100; i++) 
 	{
 		len = checklen(i);
 		d_arr(D_arr, len, i);
-		cou = count(D_arr, len);
+		cou += count(D_arr, len);
 		arrclear(D_arr, 10);
 	}
 	printf("1~100共有%d个9\n", cou);
